b5da6a370f93e023dcb5844f288c07a1119e5666
simonatorw <simonatorw@gmail.com>
2020-05-10 14:00:21 -0400

    Overriding module and some api

--- src/components/ToDo/ToDoApi.test.js
+++ src/components/ToDo/ToDoApi.test.js
@@ -1,10 +1,19 @@
 import { add, remove } from './ToDoApi';
 
+jest.mock('./ToDoApi', () => {
+  const originalModule = jest.requireActual('./ToDoApi');
+  return {
+    ...originalModule,
+    add: jest.fn(),
+  };
+});
+
 describe('ToDoApi.js', () => {
   let toDos;
 
   beforeEach(() => {
     toDos = [1, 2];
+    jest.resetAllMocks();
   });
 
   test('add', () => {
@@ -12,9 +21,21 @@ describe('ToDoApi.js', () => {
     const setValState = jest.fn();
     const evt = { keyCode: 13, target: { value: ''}};
 
+    add.mockImplementationOnce((val, todos, setf) => setf([ ...todos, val ]));
     add('foo', toDos, setToDoState, setValState, evt);
 
     expect(toDos).toEqual([1, 2, 'foo']);
+  });
+
+  test('add2', () => {
+    const setToDoState = (val) => toDos = val;
+    const setValState = jest.fn();
+    const evt = { keyCode: 13, target: { value: ''}};
+
+    add.mockImplementationOnce((val, todos, setf) => setf([ ...todos, 'duh' ]));
+    add('foo', toDos, setToDoState, setValState, evt);
+
+    expect(toDos).toEqual([1, 2, 'duh']);
   });
 
   test('remove', () => {
